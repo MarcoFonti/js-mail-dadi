6b12716d5f0645efcdf022c6a55679f0f1dae857
MarcoFonti <theemporiumg@gmail.com>
2023-12-21 17:52:36 +0100

    add dice game

--- js/script-1.js
+++ js/script-1.js
@@ -1,2 +1,40 @@
 // Check 
 console.log('JS OK DADI');
+
+//Prendo gli elementi id
+const player1Element = document.getElementById('player1');
+const player2Element = document.getElementById('player2');
+const playerVincElement = document.getElementById('playerVinc');
+
+// Verifco in Console
+console.log(player1Element, player2Element);
+
+// Creo le variabili Min e Max
+let min = 1
+let max = 6
+
+// Numeri Random 
+const randomPlayer1 = Math.floor(Math.random()*(max + 1 -min)) + min;
+const randomPlayer2 = Math.floor(Math.random()*(max + 1 -min)) + min;
+
+// Verifico in Console
+console.log(randomPlayer1, randomPlayer2);
+
+// Metto in Pagina
+player1Element.innerHTML = randomPlayer1;
+player2Element.innerHTML = randomPlayer2
+
+// Creo la variabile vuota 
+let vincent = '';
+
+// Creo le condizioni 
+if (randomPlayer1 > randomPlayer2) {
+    vincent = 'PLAYER 1'
+} else if (randomPlayer2 > randomPlayer1) {
+    vincent = 'PLAYER 2'
+} else if (randomPlayer1 === randomPlayer2){
+    vincent = 'PAREGGIO'
+}
+
+// Metto in pagina il risultato
+playerVincElement.innerText = vincent;
--- textPassages.md
+++ textPassages.md
@@ -1,1 +1,24 @@
 # PASSAGGI
+
+## GIOCO DADI
+1 Prendo gli elementi id
+
+2 Verifco in Console
+
+3 Creo le variabili Min e Max
+
+4 Numeri Random 
+
+
+5 Verifico in Console
+
+
+6 Metto in Pagina
+
+7 Creo la variabile vuota 
+
+
+8 Creo le condizioni 
+
+
+9 Metto in pagina il risultato
